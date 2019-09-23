a1fab2c9df4eff8246fc7847fce288357b5e2cad
believelody <believelody@gmail.com>
2019-09-23 08:09:30 +0200

    added main reducers in react context

---
+++ src/api/index.js
@@ -0,0 +1,18 @@
+import Strapi from 'strapi-sdk-javascript'
+import brand from './variant'
+import brew from './product'
+import brew from './profile'
+import brew from './shipping'
+import user from './user'
+import order from './order'
+
+const apiUrl = process.env.NODE_ENV === 'production' ? process.env.SERVER_URL : 'localhost:1337'
+export const strapi = new Strapi(apiUrl)
+
+export const query = obj => strapi.request('POST', '/graphql', { data: obj })
+export const register = (name, email, password) => strapi.register(name, email, password)
+export const login = (email, password) => strapi.login(email, password)
+export const create = (path, data) => strapi.createEntry(path, data)
+export const send = obj => strapi.request('POST', '/email', {data: obj})
+
+export default {order, product, profile, shipping, user, variant}
--- src/context/index.js
+++ src/context/index.js
@@ -1,5 +1,11 @@
 import React, { createContext, useContext, useReducer } from 'react'
 import { createBrowserHistory } from "history"
+import { loadingReducer, initLoadingState } from '../reducers/loadingReducer'
+import { cartReducer, initCartState } from '../reducers/cartReducer'
+import { authReducer, initAuthState } from '../reducers/authReducer'
+import { toastReducer, initToastState } from '../reducers/toastReducer'
+import { modalReducer, initModalState } from '../reducers/modalReducer'
+import { checkoutReducer, initCheckoutState } from '../reducers/checkoutReducer'
 
 const history = createBrowserHistory({
   forceRefresh: true
@@ -10,9 +16,17 @@ export const AppContext = createContext()
 export AppProvider = ({ children }) => (
   <AppContext.Provider
     value={{
+      useLoading: useReducer(loadingReducer, initLoadingState),
+      useCart: useReducer(cartReducer, initCartState),
+      useAuth: useReducer(authReducer, initAuthState),
+      useToast: useReducer(toastReducer, initToastState),
+      useModal: useReducer(modalReducer, initModalState),
+      useCheckout: useReducer(checkoutReducer, initCheckoutState)
       history
     }}
   >
     { children }
   </AppContext.Provider>
 )
+
+export const useAppHooks = () => useContext(AppContext)
