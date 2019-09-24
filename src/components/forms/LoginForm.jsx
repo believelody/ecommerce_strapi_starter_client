8f425b4152bdff8da55c0f5595f4d6784ced4cca
believelody <believelody@gmail.com>
2019-09-24 13:11:32 +0200

    implemented error effect

--- src/components/forms/LoginForm.jsx
+++ src/components/forms/LoginForm.jsx
@@ -1,25 +1,41 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { NavLink } from 'react-router-dom'
-import { Pane, Card, Text, Button } from 'evergreen-ui'
+import { Pane, Card, Text, Button, Alert } from 'evergreen-ui'
 import InputComponent from '../inputs/InputComponent'
 import FieldComponent from '../fields/FieldComponent'
 import Label from '../label/Label'
+import { useAppHooks } from '../../context'
+import { SUCCESS_AUTH, ERROR_AUTH, RESET_ERROR } from '../../reducers/authReducer'
 
 const LoginForm = () => {
-  const [username, setUsername] = useState()
-  const [email, setEmail] = useState()
-  const [errors, setErrors] = useState(null)
+  const { useAuth } = useAppHooks()
+  const [{errors}, dispatchAuth] = useAuth
 
-  const handleName = e => setUsername(e.target.value)
+  const [email, setEmail] = useState()
+  const [password, setPassword] = useState()
+  // const [errors, setErrors] = useState(null)
+
   const handleEmail = e => setEmail(e.target.value)
+  const handlePassword = e => setPassword(e.target.value)
 
   const handleSubmit = e => {
     e.preventDefault()
-    setErrors(null)
+    dispatchAuth({ type: RESET_ERROR })
     if (!email) {
-      setErrors({ email: 'Email is required' })
+      dispatchAuth({ type: ERROR_AUTH, payload: {mail: 'Email is required'}})
+    }
+    if (!password) {
+      // dispatchAuth({ type: ERROR_AUTH, payload: {password: 'Password is required'}})
+      dispatchAuth({ type: ERROR_AUTH, payload: {authFailed: 'Incorrect password'}})
+    }
+    try {
+
+    } catch (e) {
+      dispatchAuth({ type: ERROR_AUTH, payload: {authFailed: e.message}})
     }
   }
+
+  useEffect(() => {}, [errors])
 
   return (
     <Card
@@ -47,9 +63,15 @@ const LoginForm = () => {
             type='password'
             placeholder='enter your password here'
             hint='It must contain at least 6 characters, 1 numerical'
-            handleChange={handleEmail}
-            error={errors && errors.email}
+            handleChange={handlePassword}
+            error={errors && errors.password}
           />
+          {
+            errors && errors.authFailed &&
+            <Pane marginBottom={10}>
+              <Alert title={errors.authFailed} intent='danger' />
+            </Pane>
+          }
           <Button appearance='primary'>Login</Button>
         </form>
       </Pane>
