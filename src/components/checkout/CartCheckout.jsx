1fd8a96ebe0df34ed378e2b12add92459bd0c08b
believelody <believelody@gmail.com>
2019-10-10 23:40:39 +0200

    designed billing and shipping adress form component

--- src/components/checkout/CartCheckout.jsx
+++ src/components/checkout/CartCheckout.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 import { Pane } from 'evergreen-ui'
 import Accordion from '../accordions/Accordion'
 import CartList from '../cart/CartList'
+import EmptyCart from '../cart/EmptyCart'
 import Label from '../label/Label'
+import { useAppHooks } from '../../context'
 
 const CartCheckout = ({index, currentIndex, setIndex}) => {
+  const { useCart } = useAppHooks()
+  const [{cart}, dispatchCart] = useCart
+
   return (
     <Pane
       width='100%'
@@ -16,7 +21,11 @@ const CartCheckout = ({index, currentIndex, setIndex}) => {
         currentIndex={currentIndex}
         setIndex={setIndex}
         header={<Label name='Your items' />}
-        content={<CartList />}
+        content={
+          cart.length > 0 ?
+          <CartList /> :
+          <EmptyCart />
+        }
         scrollAuto
         predefinedHeight={500}
       />
